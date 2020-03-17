dcdd9e43dfc7f1cb1766b1dda99e7836faaf9913
Johnny Stene <jhonnystene@gmail.com>
2020-03-17 11:31:44 -0600

    Chicken no longer spawns close to edge

--- Chicken.gd
+++ Chicken.gd
@@ -2,8 +2,8 @@ extends Area2D
 
 func _on_Chicken_body_entered(body):
 	if("NicCage" in body.name):
-		position.x = rand_range(0, 1025)
-		position.y = rand_range(0, 601)
+		position.x = rand_range(100, 925)
+		position.y = rand_range(100, 501)
 		body.score += 1
 		if(body.score % 3 == 0):
 			if(get_parent().get_node("CeleryManager").circling):
