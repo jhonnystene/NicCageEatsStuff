08b457ab40834f13777be7613c6cb971b9df972e
Johnny Stene <johnny@stene.xyz>
2023-06-18 14:43:15 -0600

    celery no longer spawn too close to nic

--- CeleryManager.gd
+++ CeleryManager.gd
@@ -4,6 +4,7 @@ var circling = false
 
 func _process(delta):
 	var score = get_parent().get_node("NicCage").score
+	var nic_cage_position = get_parent().get_node("NicCage").position
 	var maxCeleryCount = 0
 	
 	maxCeleryCount = floor((score / 4) + 1)
@@ -12,4 +13,10 @@ func _process(delta):
 		var instance = load("res://Celery.tscn").instantiate()
 		instance.position.x = randf_range(0, 1025)
 		instance.position.y = randf_range(0, 601)
-		add_child(instance)
+		
+		if(abs(abs(instance.position.x) - abs(nic_cage_position.x)) < 50):
+			pass
+		elif(abs(abs(instance.position.y) - abs(nic_cage_position.y)) < 50):
+			pass
+		else:
+			add_child(instance)
