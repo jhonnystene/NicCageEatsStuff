002ac5c0de888239c54c4ec088d3d1230a2fbdd8
Johnny Stene <johnny@stene.xyz>
2023-09-28 11:42:54 -0600

    Eliminate unneeded token check

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,15 +2,13 @@ extends Node2D
 
 func _process(_delta):
 	if(WebServicesController.token):
+		$UI/SignedInLabel.text = "Signed in as " + WebServicesController.username
 		if(Global.highest_score):
 			$UI/ScoreLabel.text = "Score: " + str($NicCage.score) + " (high score: " + str(Global.highest_score) + ")"
 		else:
 			$UI/ScoreLabel.text = "Score: " + str($NicCage.score)
 	else:
+		$UI/SignedInLabel.text = "Not signed in - score will not be saved"
 		$UI/ScoreLabel.text = "Score: " + str($NicCage.score) + " (not signed in - score won't be saved)"
 	$UI/LivesLabel.text = "Lives: " + str($NicCage.lives)
-	
-	if(WebServicesController.token):
-		$UI/SignedInLabel.text = "Signed in as " + WebServicesController.username
-	else:
-		$UI/SignedInLabel.text = "Not signed in - score will not be saved"
+		
